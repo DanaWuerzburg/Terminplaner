33eae6e6224e13ceff58f91317f3c17c3248daf0
Dana Wuerzburg <Miky_green-eis@web.de>
2011-11-08 13:31:26 +0100

    Create rails
    rails new Terminplaner
    kreiert ein neues RailsProject

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Terminplaner::Application.config.secret_token = 'd22cc21dd6f0ace47f37552e096c87108dd31185868777e60219a9090cf97cf8e4013e4f0a057598706c586854f926c4d0e1516fc4b3e4a2c64dfb65d8e6ad0c'
