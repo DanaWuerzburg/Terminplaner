4ff403c24329e45e1b22762232b21f9a579089dd
Dana Wuerzburg <Miky_green-eis@web.de>
2011-12-03 01:14:27 +0100

    Login Screen ist jetzt Startseite

--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,10 @@ Terminplaner::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'appointments#index'
+
+  #            rm public/index.html # in der console
+  #root :to => 'appointments#index'
+  root :to => 'user_sessions#new'
 
   # See how all your routes lay out with "rake routes"
 
