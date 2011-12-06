30d9f9d9cbbd94006b3c4f74097d754d6baab8b0
Christo Phillm <schrisse@gmail.com>
2011-12-06 23:21:45 +0100

    Gitignore

--- db/migrate/20111120222231_add_fields_to_appointment.rb
+++
@@ -1,5 +0,0 @@
-class AddFieldsToAppointment < ActiveRecord::Migration
-  def change
-    add_column :appointments, :priority_number, :integer, :group
-  end
-end
--- db/migrate/20111206165246_add_group_type_to_appointments.rb
+++
@@ -1,5 +0,0 @@
-class AddGroupTypeToAppointments < ActiveRecord::Migration
-  def change
-    add_column :appointments, :grouptype, :group
-  end
-end
