fca3e0e8c126e2d749edac062454d943600c17b7
Christoph <schrisse@gmail.com>
2012-02-01 23:16:55 +0100

    Kalenderansicht funktioniert Tschakka! :)

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -36,7 +36,10 @@ class AppointmentsController < ApplicationController
     @shared_friendship = FriendshipAppointment.new
     @shared_friendships = FriendshipAppointment.find(:all, :conditions => {:user_id => current_user})
 
-
+    @month = (params[:month] || (Time.zone || Time).now.month).to_i
+    @year = (params[:year] || (Time.zone || Time).now.year).to_i
+    @shown_month = Date.civil(@year, @month)
+    @event_strips = Appointment.event_strips_for_month(@shown_month)
 
 
    #ordnet inhalte je nach spalte
