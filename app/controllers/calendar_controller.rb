c2e7d3345859768b7cb8772def93c6b67c80ae8c
Christoph <schrisse@gmail.com>
2012-02-01 17:27:30 +0100

    layout bugfixes calendar, users, friendships

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,6 +1,9 @@
 class CalendarController < ApplicationController
   
   def index
+    @group = Group.new
+    @groups = Group.find(:all, :conditions => {:user_id => current_user})
+
     @month = (params[:month] || (Time.zone || Time).now.month).to_i
     @year = (params[:year] || (Time.zone || Time).now.year).to_i
 
