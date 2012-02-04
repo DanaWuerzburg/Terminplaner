f1a37ff1eab3abdc37c77c29aed6750ce1a3fda0
Christoph <schrisse@gmail.com>
2012-02-04 11:54:21 +0100

    In Kalenderasicht werden nur eigene Appointments angezeigt

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -38,7 +38,7 @@ class AppointmentsController < ApplicationController
     @month = (params[:month] || (Time.zone || Time).now.month).to_i
     @year = (params[:year] || (Time.zone || Time).now.year).to_i
     @shown_month = Date.civil(@year, @month)
-    @event_strips = Appointment.event_strips_for_month(@shown_month)
+    @event_strips = Appointment.event_strips_for_month(@shown_month, :include => :user, :conditions => {:user_id => current_user})
 
 
    #ordnet inhalte je nach spalte
--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,4 +1,6 @@
 module CalendarHelper
+
+
   def month_link(month_date)
     link_to(I18n.localize(month_date, :format => "%B"), {:month => month_date.month, :year => month_date.year})
   end
@@ -9,9 +11,9 @@ module CalendarHelper
       :year => @year,
       :month => @month,
       :event_strips => @event_strips,
-      :month_name_text => I18n.localize(@shown_month, :format => "%B %Y"),
-      :previous_month_text => "<< " + month_link(@shown_month.prev_month),
-      :next_month_text => month_link(@shown_month.next_month) + " >>",
+      :month_name_text => "<h4>" + I18n.localize(@shown_month, :format => "%B %Y") + "</h4>",
+      :previous_month_text => month_link(@shown_month.prev_month),
+      :next_month_text => month_link(@shown_month.next_month),
       :use_all_day => true
     }
   end
@@ -19,11 +21,20 @@ module CalendarHelper
   def event_calendar
     # args is an argument hash containing :event, :day, and :options
     calendar event_calendar_opts do |args|
-      event, day = args[:event], args[:day]
-      html = %(<a href="/appointments/#{event.id}/edit" title="#{h(event.note)}">)
-      html << display_event_time(event, day)
-      html << %(#{h(event.note)}</a>)
-      html
+      if current_user.id.eql?(args[:event].user_id)
+        event, day, options = args[:event], args[:day], args[:options]
+        html = %(<a href="/appointments/#{event.id}/edit" title="#{h(event.note)}">)
+        html << display_event_time(event, day)
+        html << %(#{h(event.note)}</a>)
+        html
+
+      else
+        event, day = args[:event], args[:day]
+        html = %(#{h(event.user_id)})
+        html << %(#{h(options)})
+
+      end
+
     end
   end
 end
